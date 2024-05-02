15a3d3c905fa9cc340be8ba96a09483a489b7eab
toretatenee <toretatenee@gmail.com>
2024-05-02 13:23:48 +0900

    ハッシュ値の計算方法が変わったので対応

--- PrincessTool/Works/RenameHashedFile.cs
+++ PrincessTool/Works/RenameHashedFile.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Data.SQLite;
 using System.Collections.Generic;
 using System.Security.Cryptography;
+using System.IO.Hashing;
+using System.Diagnostics;
+
 
 namespace AioiLight.PrincessTool.Works
 {
@@ -45,6 +48,39 @@ namespace AioiLight.PrincessTool.Works
             new Progress("Rename hashed files", filesAmount, Process);
         }
 
+        private string HashSha1( string item ) {
+            // SHA1ハッシュの計算。
+            var fileStream = new FileStream(item,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.None);
+            var md5 = MD5.Create();
+            var fileMD5 = md5.ComputeHash(fileStream);
+            var hash = BitConverter.ToString(fileMD5).ToLower().Replace("-", "");
+
+            // 後片付け
+            md5.Clear();
+            fileStream.Close();
+            return hash;
+        }
+
+        private string HashXXHash64( string item ) {
+            // SHA1ハッシュの計算。
+            var fileStream = new FileStream(item,
+                FileMode.Open,
+                FileAccess.Read,
+                FileShare.None);
+
+            var xxhash = new XxHash64();
+            xxhash.Append(fileStream);
+            var hash64 = xxhash.GetCurrentHash();
+            var hash = BitConverter.ToString(hash64).ToLower().Replace("-", "");
+
+            // 後片付け
+            fileStream.Close();
+            return hash;
+        }
+
         public void Process(IProgress<int> progress)
         {
             var files = Directory.GetFiles(Program.Dest, "*.*", SearchOption.AllDirectories);
@@ -70,18 +106,9 @@ namespace AioiLight.PrincessTool.Works
                     continue;
                 }
 
-                // SHA1ハッシュの計算。
-                var fileStream = new FileStream(item,
-                    FileMode.Open,
-                    FileAccess.Read,
-                    FileShare.None);
-                var md5 = MD5.Create();
-                var fileMD5 = md5.ComputeHash(fileStream);
-                var hash = BitConverter.ToString(fileMD5).ToLower().Replace("-", "");
-
-                // 後片付け
-                md5.Clear();
-                fileStream.Close();
+                // ハッシュ計算
+                //var hash = this.HashSha1( item );
+                var hash = this.HashXXHash64(item);
 
                 if (!HashList.ContainsKey(hash))
                 {
@@ -91,6 +118,7 @@ namespace AioiLight.PrincessTool.Works
                 }
 
                 // リネームする。
+                Debug.WriteLine(item + "(" + hash +")→" + HashList[hash]);
                 var result = Path.Combine(Path.GetDirectoryName(itemRelative), Path.GetFileName(HashList[hash]));
 
                 try
